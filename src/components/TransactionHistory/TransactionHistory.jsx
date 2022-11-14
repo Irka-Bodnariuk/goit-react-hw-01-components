0853eeeeb70f00763f7b0b32cb5329873ec44a13
Iryna Bodnariuk <bodnariuk.iryna1@gmail.com>
2022-11-15 00:38:08 +0200

    fix: TransactionHistory,FriendList,Statistics

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -23,3 +23,14 @@ export default function TransactionHistory({ items }) {
     </table>
   );
 }
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+};
